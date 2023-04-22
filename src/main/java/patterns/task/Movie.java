607fea2d6ae0b08cd4bff7330a632f3206541f54
Anton <sokolplus3783@gmail.com>
2023-04-22 20:27:07 +0300

    add Builder to Movie.java

--- src/main/java/patterns/task/Movie.java
+++ src/main/java/patterns/task/Movie.java
@@ -1,12 +1,20 @@
 package patterns.task;
 
+import java.util.List;
+import patterns.models.movietypes.MovieType;
+import patterns.models.Person;
+
 public class Movie {
-    private final String title;
+    private String title;
+
+    private int id;
+    private String country;
+    private String description;
+    private Person director;
+    private List<Person> actors;
     private MovieType priceCode;
 
-    public Movie(String title, MovieType priceCode) {
-        this.title = title;
-        this.priceCode = priceCode;
+    private Movie(){
     }
 
     public MovieType getPriceCode() {
@@ -16,5 +24,78 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    public static class Builder {
+
+        private String title;
+        private String country;
+        private String description;
+        private Person director;
+        private List<Person> actors;
+
+        private MovieType priceCode;
+
+        private int id;
+
+        public Builder(Movie movie){
+            title = movie.title;
+            country = movie.country;
+            actors = movie.actors;
+            director = movie.director;
+            description = movie.description;
+            priceCode = movie.priceCode;
+        }
+
+        public Builder() {
+
+        }
+
+        public Movie build(){
+            Movie movie = new Movie();
+            movie.title = this.title;
+            movie.country = this.country;
+            movie.actors = this.actors;
+            movie.director = this.director;
+            movie.description = this.description;
+            return movie;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder country(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder priceCode(MovieType priceCode) {
+            this.priceCode = priceCode;
+            return this;
+        }
+
+        public Builder director(Person director) {
+            this.director = director;
+            return this;
+        }
+
+        public Builder actors(List<Person> actors) {
+            this.actors = actors;
+            return this;
+        }
+
+    }
+
 
 }
