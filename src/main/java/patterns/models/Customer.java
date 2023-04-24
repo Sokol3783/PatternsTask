9f60b4236a2c923428683fc930629f82756f464e
Anton <sokolplus3783@gmail.com>
2023-04-24 03:03:25 +0300

    remove comment

--- src/main/java/patterns/models/Customer.java
+++ src/main/java/patterns/models/Customer.java
@@ -11,31 +11,11 @@ public class Customer {
         this.id = id;
     }
 
-    public String getName() {
+   public String getName() {
         return name;
     }
 
   public int getId() {
     return id;
   }
-
-  /*
-  public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = each.getAmount();
-            frequentRenterPoints += each.getFrequentRenterPoints();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
-        }
-        //add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
-        return result;
-    }
-   */
-
 }
